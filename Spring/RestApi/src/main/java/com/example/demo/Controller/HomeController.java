690897bbd6da8d58149c808a91ad16f58b584ea4
mohammed-fazil-H <74044065+mohammed-fazil-H@users.noreply.github.com>
2024-01-02 10:22:36 +0530

    Add files via upload

---
+++ Spring/RestApi/src/main/java/com/example/demo/Controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	@ResponseBody
+	public String display()
+	{
+		return "Hi Heloo";
+	}
+
+}
---
+++ Spring/RestApi/src/main/java/com/example/demo/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.Entity.Employee;
+
+@Service
+public class EmployeeService {
+	
+	
+	List <Employee> e=new ArrayList<>();
+
+}
